776de24cb49256f8e905670a81d70e6b11e3435d
Terio <teriohachula@gmail.com>
2023-07-22 13:19:34 +0200

    maybe that?

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,5 +4,13 @@ export default defineNuxtConfig({
   css: [
     '~/assets/css/main.css',
     '~/assets/css/fonts.css'
-  ]
+  ],
+  app: {
+    head: {
+      links: [
+        { rel: 'stylesheet', href: '/assets/css/main.css' },
+        { rel: 'stylesheet', href: '/assets/css/fonts.css' },
+      ]
+    }
+  }
 })
