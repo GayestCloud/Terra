0920967ec6a52710abcdbf2e4506f6a34a15ff93
TerioDev <terihachula@gmail.com>
2023-12-06 21:39:19 +0100

    Index update

--- js/index.js
+++ js/index.js
@@ -2,6 +2,11 @@ function popup(name, state) {
     let el = document.getElementById(name);
     if (el === undefined)
         return;
+
+    let body = document.getElementsByTagName('body')[0];
+    if (body) {
+        body.classList.toggle('popup_active', state);
+    }
     
     el.classList.toggle('active', state);
 }
